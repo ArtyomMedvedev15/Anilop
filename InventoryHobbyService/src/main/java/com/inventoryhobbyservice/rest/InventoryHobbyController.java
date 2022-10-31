493c0ae7f00366dd4d14f724f53ed196e2a50c48
Tim01Bro <arthaos14@gmail.com>
2022-10-31 20:33:55 +0300

    Refactor inventory controller

--- InventoryHobbyService/src/main/java/com/inventoryhobbyservice/rest/InventoryHobbyController.java
+++ InventoryHobbyService/src/main/java/com/inventoryhobbyservice/rest/InventoryHobbyController.java
@@ -6,6 +6,7 @@ import com.inventoryhobbyservice.dto.InventoryInfoRequest;
 import com.inventoryhobbyservice.dto.InventoryInfoResponse;
 import com.inventoryhobbyservice.service.InventoryService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +16,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/inventory")
 @AllArgsConstructor
+@Slf4j
 public class InventoryHobbyController {
 
     private InventoryService inventoryService;
-
-    @GetMapping("/verify/{sku_code}")
-    public ResponseEntity<Boolean> IsInStock(@PathVariable("sku_code")String skuCode){
-        return ResponseEntity.ok().body(true);
-    }
 
     @GetMapping("/addtoinventory/{idinventory}")
     public ResponseEntity<InventoryInfo>addHobbyToInventory(@PathVariable("idinventory")Long idinventory,
@@ -33,18 +30,38 @@ public class InventoryHobbyController {
                 .build();
         InventoryInfo inventoryInfoResponse = inventoryService.addHobbyToInventory(inventoryInfoFromDto,idinventory);
 
-        return ResponseEntity.ok().body(inventoryInfoResponse);
+        if(inventoryInfoResponse!=null) {
+            log.info("Add hobby to inventory with id inventory{}",idinventory);
+            return ResponseEntity.ok().body(inventoryInfoResponse);
+        }else{
+            log.error("This hobby already in inventory {}",idinventory);
+            return ResponseEntity.badRequest().body(null);
+
+        }
     }
 
     @GetMapping("/{userid}")
     public ResponseEntity<List<InventoryInfoResponse>>findAllInventoryInfoByUserId(@PathVariable("userid")Long userId){
-        return ResponseEntity.ok().body(inventoryService.findAllInventoryByUserInventoryId(userId));
+       List<InventoryInfoResponse> hobbyListInventory = inventoryService.findAllInventoryByUserInventoryId(userId);
+        if(hobbyListInventory!=null){
+            log.info("Find all hobby in inventory user with id {}",userId);
+            return ResponseEntity.ok().body(hobbyListInventory);
+        }else {
+            log.error("Inventory with id {} doesn't exists",userId);
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<Void>deleteInventoryInfo(@RequestBody InventoryInfoDeleteRequest inventoryInfoDeleteRequest){
-        inventoryService.deleteInventory(inventoryInfoDeleteRequest.getSerial_id(),inventoryInfoDeleteRequest.getUserInventoryId());
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        Boolean isDelete = inventoryService.deleteInventory(inventoryInfoDeleteRequest.getSerial_id(), inventoryInfoDeleteRequest.getUserInventoryId());
+        if(isDelete) {
+            log.warn("Hobby with id {} was delete from inventory",inventoryInfoDeleteRequest.getSerial_id());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }else{
+            log.error("Inventory with id {} doesn't exists",inventoryInfoDeleteRequest.getUserInventoryId());
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
 }
