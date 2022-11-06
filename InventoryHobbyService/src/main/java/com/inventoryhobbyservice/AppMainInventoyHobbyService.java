193338be1264d07fac2f79abfff5bb225a8d7a96
Tim01Bro <arthaos14@gmail.com>
2022-11-06 16:16:00 +0300

    Add eureka client

--- InventoryHobbyService/src/main/java/com/inventoryhobbyservice/AppMainInventoyHobbyService.java
+++ InventoryHobbyService/src/main/java/com/inventoryhobbyservice/AppMainInventoyHobbyService.java
@@ -2,8 +2,10 @@ package com.inventoryhobbyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AppMainInventoyHobbyService {
     public static void main(String[] args) {
         SpringApplication.run(AppMainInventoyHobbyService.class,args);
